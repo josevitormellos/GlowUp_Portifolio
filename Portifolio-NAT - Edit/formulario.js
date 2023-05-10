c043b1a5ba3d8200c843169dbca2306ef7bb80b7
josevitormellos <95095644+josevitormellos@users.noreply.github.com>
2023-05-10 11:06:49 -0300

    Atualizado
    Existente

---
+++ Portifolio-NAT - Edit/formulario.js
@@ -0,0 +1,59 @@
+function CriarFormulario() {
+    const form = document.getElementById("root");
+    const status = document.getElementById("statusroot");
+    if (form.innerHTML == ``) {
+        status.innerHTML = 
+        `<div>   
+        <h1>Contato</h1>
+        </div>
+        <div>
+            <p>Preencha o formulário de contato.</p>
+            <p>Se preferir, use um dos canais indicados abaixo.</p>
+            <p>Será um prazer falar com você!</p>
+        </div>
+        <div>
+        <p><b>Telefone:</b> (21) 99894-3822</p>
+        <p><b>Email:</b> nataliamello@hotmail.com</p>
+        </div>`;
+    form.innerHTML =
+        `<div>   
+            <input type="text" id="nome" name="nome" required placeholder="Nome">
+            <input type="text" id="sobrenome" name="sobrenome" required placeholder="Sobrenome">
+            </div>
+            <div>
+            <input type="text" id="email" name="email" required placeholder="Email">
+            <input type="text" id="assunto" name="assunto" required placeholder="Assunto">
+            </div>
+            <div>
+            <input type="text" id="empresa" name="empresa" required placeholder="Empresa">
+            <input type="text" id="cell" name="cell" required placeholder="Telefone com DDD">
+            </div>
+            <textarea cols="30" rows="5" placeholder="Escreva sua mensagem aqui..."></textarea>
+            <button type="submit">Enviar</button>`;
+    }
+    else {
+        status.innerHTML = ``;
+        form.innerHTML = ``;
+    }
+    
+}
+
+function EnviarEmail() {
+    var nome = document.getElementById('nome').value;
+	var telefone = document.getElementById('cell').value;
+
+	var email = document.getElementById('email').value;
+
+	var corpoEmail = 'Nome: ' + nome + '%0D%0A';
+	corpoEmail += 'Telefone: ' + telefone + '%0D%0A';
+    corpoEmail += 'E-mail: ' + email + '%0D%0A';
+    corpoEmail += 'Desejo entrar em contato com você para mais informações dos serviços';
+
+	var linkEmail = 'mailto:nataliamello@hotmail.com';
+	linkEmail += '?subject=Contato pelo Formulário do GlowUP';
+	linkEmail += '&body=' + corpoEmail;
+
+
+
+	window.location.href = linkEmail;
+}
