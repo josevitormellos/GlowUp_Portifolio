a58c5ea60ddfa5c02a3ee5ecbddba2cd13c0e343
josevitormellos <95095644+josevitormellos@users.noreply.github.com>
2023-06-08 20:29:43 -0300

    Esperando O texto. Processo COMPLETO

--- Portifolio-NAT - Edit/formulario.js
+++ Portifolio-NAT - Edit/formulario.js
@@ -1,56 +1,32 @@
 function CriarFormulario() {
-    const form = document.getElementById("root");
-   /*const status = document.getElementById("statusroot");*/
-    if (form.innerHTML == ``) {
-        /*status.innerHTML = 
-        `<div>   
-        <h1>Contato</h1>
-        </div>
-        <div>
-            <p>Preencha o formulário de contato.</p>
-            <p>Se preferir, use um dos canais indicados abaixo.</p>
-            <p>Será um prazer falar com você!</p>
-        </div>
-        <div>
-        <p><b>Telefone:</b> (21) 99894-3822</p>
-        <p><b>Email:</b> nataliamello@hotmail.com</p>
-        </div>`;*/
-    form.innerHTML =
-        `  
-            <input type="text" id="nome" name="nome" required placeholder="Nome">
-            <input type="text" id="email" name="email" required placeholder="Email">
-            <input type="text" id="empresa" name="empresa" required placeholder="Empresa">
-            <input type="text" id="cell" name="cell" required placeholder="Telefone com DDD">
-            <textarea cols="30" rows="5" placeholder="Escreva sua mensagem aqui..."></textarea>
-            <button class="btn-1" type="submit"><spam>Enviar</spam></button>`;
-    }
-    else {
-        /*status.innerHTML = ``;*/
-        form.innerHTML = ``;
-    }
-    
+    const form = document.getElementById("formMensagem");
+    form.style.visibility = "visible";
+    const forms = document.getElementById("formObrigatorio");
+    const feito = document.getElementById('posFeito');
+    forms.style.display="flex";
+    feito.style.display= "none";
+}
+
+function FecharFormulario(){
+    const form = document.getElementById("formMensagem");
+    form.style.visibility = "hidden";
 }
 
 function EnviarEmail() {
-    var nome = document.getElementById('nome').value;
-	var telefone = document.getElementById('cell').value;
-
-	var email = document.getElementById('email').value;
-
-	var corpoEmail = 'Nome: ' + nome + '%0D%0A';
-	corpoEmail += 'Telefone: ' + telefone + '%0D%0A';
-    corpoEmail += 'E-mail: ' + email + '%0D%0A';
-    corpoEmail += 'Desejo entrar em contato com você para mais informações dos serviços';
-
-	var linkEmail = 'mailto:contato.glowupfinanceiro@gmail.com';
-	linkEmail += '?subject=Contato pelo Formulário do GlowUP';
-	linkEmail += '&body=' + corpoEmail;
-
-
-
-	window.location.href = linkEmail;
+    const feito = document.getElementById('posFeito');
+   
+    const form = document.getElementById("formObrigatorio");
+	
+    
+    form.style.display="none";
+    feito.style.display= "flex";
+	
 }
 
 function Sobremin(){
     var nome = document.getElementById('nome');
+}
+function openInNewTab(url){
+    var win = window.open(url, '_blank');
+  win.focus();
 }
